19ba0e3f3cfc83867d64e05eb77b1150b32d5e11
Nhut Phan <thanquan@gmail.com>
2025-10-17 14:18:36 +0700

    Update

--- lib/livekit_protocol_ex.ex
+++ lib/livekit_protocol_ex.ex
@@ -1,5 +1,14 @@
 defmodule LivekitProtocolEx do
   use Protox,
-    files: ["./priv/protobufs/livekit_rtc.proto", "./priv/protobufs/livekit_agent.proto"],
-    namespace: __MODULE__
+    files: [
+      "./priv/protobufs/livekit_rtc.proto",
+      "./priv/protobufs/livekit_agent.proto",
+      "./priv/protobufs/infra/link.proto",
+      "./priv/protobufs/rpc/agent.proto",
+      "./priv/protobufs/rpc/room.proto",
+      "./priv/protobufs/rpc/io.proto"
+    ],
+    paths: [
+      "./priv/protobufs"
+    ]
 end
---
+++ lib/livekit_protocol_ex/agent.ex
@@ -0,0 +1,4 @@
+defmodule LivekitProtocolEx.Agent do
+  use Protox,
+    files: ["./priv/protobufs/agent/livekit_agent_session.proto"]
+end
